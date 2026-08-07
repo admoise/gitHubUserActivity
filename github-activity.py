d620b96e895a72682de8c846301282330eb52e1e
admoise <admoises@gmail.com>
2026-08-06 18:09:05 -0700

    Fist release

--- github-activity.py
+++ github-activity.py
@@ -17,17 +17,33 @@ def parse_activity(response):
         sys.exit(1)
     # Extract all actions from the response
     try:
-        actions=[]
-        #actions = [(event["type"], event["repo"]["name"]) for event in response]
-        for event in response:
-            if "action" in event["payload"].keys():
-                actions.append([event["payload"]["action"], event["repo"]["name"]])
-
+        actions = [(event["type"], event["repo"]["name"]) for event in response]
         # Get the action of the very first event
         #first_action = response[0]["type"]
-        return actions
     except KeyError:
         print("key error")
+        sys.exit(1)
+
+    tuple_counts= {}
+    for action in actions:
+        if action in tuple_counts:
+            tuple_counts[action] +=1
+        else:
+            tuple_counts[action] = 1
+    for key in tuple_counts.keys():
+        if key[0] == "PushEvent":
+            print("Pushed  ",tuple_counts[key],"commits to ",key[1])
+        if key[0] == "CreateEvent":
+            print("Created ",tuple_counts[key],"new breanch(es): ",key[1])
+        if key[0] == "IssuesEvent":
+            print("Opened  ",tuple_counts[key],"new issue(s) to ",key[1])
+        if key[0] == "PullRequestEvent":
+            print("Created ",tuple_counts[key], "pull request(s) to ",key[1])
+        if key[0] == "PublicEvent":
+            print("Changed ",key[1], "repository to public")
+    #print(tuple_counts.keys())
+
+
 
 def load_json_for_test():
     if not os.path.exists("test.json"):
@@ -53,8 +69,6 @@ def main():
     if len(args) >1:
         print("provide just one username")
         sys.exit(1)
-    #print(get_activity(username))
-    #print(parse_activity(load_json_for_test()))
-    print(parse_activity(get_activity(username)))
+    parse_activity(get_activity(username))
 if __name__ == "__main__":
     main()
